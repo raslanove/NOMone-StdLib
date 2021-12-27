c9c812716d0ccdd50057cb8bd94a79d6651a5336
Omar El Sayyed <omar@nomone.com>
2021-12-27 02:56:39 +0200

    - Changed the shared library name to be application independent.

--- Backends/Android/Java/com/nomone/std_lib/StdLibNatives.java
+++ Backends/Android/Java/com/nomone/std_lib/StdLibNatives.java
@@ -2,7 +2,7 @@ package com.nomone.std_lib;
 
 public class StdLibNatives {
 
-    static { System.loadLibrary("HelloCC"); }
+    static { System.loadLibrary("NOMoneStdLib"); }
 
     // Necessary to trigger running the static code. Could also use Class.forName instead of calling a method.
     public static void start() {}
